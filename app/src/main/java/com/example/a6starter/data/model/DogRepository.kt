d7cc36fd4a9a75134d4c625326e0ca6ed5eec813
zachseidner1 <zachary.seidner@gmail.com>
2024-11-16 14:25:51 -0500

    Inject dogBreedApi

--- app/src/main/java/com/example/a6starter/data/model/DogRepository.kt
+++ app/src/main/java/com/example/a6starter/data/model/DogRepository.kt
@@ -1,12 +1,15 @@
 package com.example.a6starter.data.model
 
 import com.example.a6starter.data.entities.DogEntity
+import com.example.a6starter.data.remote.DogBreedApi
 import retrofit2.Response
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DogRepository @Inject constructor() {
+class DogRepository @Inject constructor(
+    dogBreedApi: DogBreedApi,
+) {
     suspend fun getDogBreeds(pageNumber: Int): Response<DogEntity> {
         TODO("Implement this function")
     }
