2a2316b6b4b0c3e27e1cf8eaacd63709aeb41d5f
zachseidner1 <zachary.seidner@gmail.com>
2024-11-16 15:33:14 -0500

    Clarify page number

--- app/src/main/java/com/example/a6starter/data/remote/DogBreedApi.kt
+++ app/src/main/java/com/example/a6starter/data/remote/DogBreedApi.kt
@@ -2,4 +2,8 @@ package com.example.a6starter.data.remote
 
 interface DogBreedApi {
     // TODO specify your API
+    //  This time you will want your function to take in a parameter for the page number, since we
+    //  are dealing with paginated data.
+    //  Annotate the parameter with @Query("page[number]") to tell Retrofit how to put the parameter
+    //  in the API Request.
 }
