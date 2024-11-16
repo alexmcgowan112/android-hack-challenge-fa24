7416c2f77f5d7239162b58c76c5a04c5aa503447
zachseidner1 <zachary.seidner@gmail.com>
2024-11-16 14:25:21 -0500

    More clear starter code

--- app/src/main/java/com/example/a6starter/di/AppModule.kt
+++ app/src/main/java/com/example/a6starter/di/AppModule.kt
@@ -15,8 +15,8 @@ import javax.inject.Singleton
  * This is the AppModule. This handles dependency injection with Dagger Hilt for you, so you do not
  * need to modify this file.
  *
- * Essentially what it does is provide a Singleton Moshi adapter and a Singleton instance of MyApi so
- * they can be used throughout the app.
+ * Essentially what it does is provide a Singleton instance of a Moshi adapter and a Singleton
+ * instance of MyApi so they can be used throughout the app.
  */
 @Module
 @InstallIn(SingletonComponent::class)
--- app/src/main/java/com/example/a6starter/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/a6starter/ui/screens/main/MainScreen.kt
@@ -1,8 +1,21 @@
 package com.example.a6starter.ui.screens.main
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+private const val LOADING_KEY = "LOADING"
 
 @Composable
 fun MainScreen(viewModel: MainScreenViewModel = hiltViewModel()) {
@@ -10,4 +23,30 @@ fun MainScreen(viewModel: MainScreenViewModel = hiltViewModel()) {
         "TODO: Create your main screen here, note that you can access the viewModel from " +
                 "the composable parameter ($viewModel)"
     )
+    val lazyListState = rememberLazyListState()
+    // To see when we need to load more data, we create a stateful variable based on the lazy list
+    //  state. It is true if our loading circle is visible.
+    val loadingCircleVisible by remember {
+        derivedStateOf {
+            lazyListState.layoutInfo.visibleItemsInfo.any { it.key == LOADING_KEY }
+        }
+    }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        // We then use snapshotFlow to convert this stateful variable into a flow, so this way
+        //  we can observe and react to its changes.
+        snapshotFlow { loadingCircleVisible }.onEach {
+            // TODO call load next page here
+        }.launchIn(coroutineScope)
+    }
+
+    LazyColumn(state = lazyListState) {
+        items(TODO("Add your list items here")) {
+
+        }
+        item(key = LOADING_KEY) {
+            CircularProgressIndicator()
+        }
+    }
 }
