fb38c113e564c5c03f2824729d852d4528a1b2d8
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2024-12-05 23:28:17 -0500

    Added TODO, ChangeD Spreferences DClass

--- app/src/main/java/com/example/a6starter/data/entities/sPreferences.kt
+++ app/src/main/java/com/example/a6starter/data/entities/sPreferences.kt
@@ -4,16 +4,18 @@ import com.squareup.moshi.Json
 // Making Assumptions Here For Now
 
 data class SPreferences(
-    val review: Boolean,
-    val homework: Boolean,
-    val morning: Boolean,
-    val afternoon: Boolean,
-    val evening: Boolean,
-    val central: Boolean,
-    val north: Boolean,
-    val west: Boolean,
-    val collegeTown: Boolean
+    val review: Boolean?,
+    val homework: Boolean?,
+    val morning: Boolean?,
+    val afternoon: Boolean?,
+    val evening: Boolean?,
+    val Central_campus: Boolean?,
+    val North_campus: Boolean?,
+    val West_campus: Boolean?,
+    val collegeTown: Boolean?
 )
+
+// TODO: Move the ones related to different MVVM to different files.
 
 data class SAccountCreate(
     val name: String,
@@ -22,6 +24,7 @@ data class SAccountCreate(
     val confirmPassword: String
 )
 
-data class SFile(
-    val file: String
+data class SAccountLogin(
+    val netId: String,
+    val password: String
 )
