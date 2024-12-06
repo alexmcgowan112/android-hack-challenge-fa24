b1e5b3f4fb0228353852e9aabdc9d4d1fdf7a610
ryanc000 <ryanchiew7@gmail.com>
2024-12-06 16:31:37 -0500

    Changed PreferencesScreenViewModel & preferencesScreen to correct format of backend

--- app/src/main/java/com/example/a6starter/ui/screens/main/viewmodels/PreferencesScreenViewModel.kt
+++ app/src/main/java/com/example/a6starter/ui/screens/main/viewmodels/PreferencesScreenViewModel.kt
@@ -1,16 +1,19 @@
 package com.example.a6starter.ui.screens.main.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a6starter.data.entities.SPreferences
+import com.example.a6starter.data.model.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class PreferencesScreenViewModel @Inject constructor(
-//    private val repository: Repository,
+    private val repository: Repository,
 ) : ViewModel() {
 
     private val preferencesFlow = MutableStateFlow(
@@ -42,6 +45,21 @@ class PreferencesScreenViewModel @Inject constructor(
             // only updates the preference of field that is passed in
         )
         preferencesFlow.value = updatedPreferences
+        viewModelScope.launch {
+            try {
+                val response = repository.updatePreferences(preferencesFlow.value)
+                if (response.isSuccessful) {
+                    println("Preferences updated successfully")
+                } else {
+
+                    println("Error updating preferences: ${response.code()}")
+                }
+            } catch (e: Exception) {
+
+                println("Error updating preferences: ${e.message}")
+            }
+        }
+
     }
 
     // TODO - Functions
